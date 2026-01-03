aaba85499c396dc34b04fbcb4e94437abd2af426
Subhayan10 <138779442+Subhayan10@users.noreply.github.com>
2026-01-03 15:30:58 +0530

    Time: 1 ms (44.54%), Space: 47.1 MB (11.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -2,8 +2,8 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int m = image.length;
         int n = image[0].length;
-        int[][] result = new int[m][n];
         boolean[][] isVisited = new boolean[m][n];
+        int[][] result = new int[m][n];
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
@@ -11,25 +11,26 @@ class Solution {
                 result[i][j]=image[i][j];
             }
         }
-        dfs(sr,sc,image,result,isVisited,m,n,color);
+        dfs(sr,sc,image,isVisited,m,n,color,result);
         return result;
     }
 
-    public void dfs(int row, int col , int[][] image, int[][] result, boolean[][] isVisited, int m, int n, int color)
+    private void dfs(int row,int col,int[][] image,boolean[][] isVisited,int m,int n,int color,int[][] result)
     {
+        result[row][col]=color;
         isVisited[row][col]=true;
-        result[row][col] = color;
-        int[] xCor = {-1,0,0,+1};
-        int[] yCor = {0,-1,+1,0};
+
+        int[] xCor = {-1,0,0,1};
+        int[] yCor = {0,-1,1,0};
 
         for(int k=0;k<4;k++)
         {
-            int newRow = row + xCor[k];
-            int newCol = col + yCor[k];
+            int newRow = xCor[k]+row;
+            int newCol = yCor[k]+col;
 
-            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && !isVisited[newRow][newCol] && image[newRow][newCol]==image[row][col])
+            if(newRow>=0 && newRow<m && newCol>=0 && newCol<n && !isVisited[newRow][newCol] && image[row][col]==image[newRow][newCol])
             {
-                dfs(newRow,newCol,image,result,isVisited,m,n,color);
+                dfs(newRow,newCol,image,isVisited,m,n,color,result);
             }
         }
     }
